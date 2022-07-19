ad18eb088365372fd55fcf2930c6f7286b2f6caf
Hessam Imen <hessam.imen@gmail.com>
2022-07-19 19:18:51 +1000

    commit tree d7ff70042b2b6b40c965552fb80d6b896ea85ac1

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,12 +1,3 @@
--- INSERT INTO bears VALUES (1,"Mr. Chocolate", 2, "M", "brown", "gentle", 1);
--- INSERT INTO bears VALUES (2,"Rowdy", 2, "F", "black", "sensitive", 1);
--- INSERT INTO bears VALUES (3,"Tabitha", 3, "M", "white", "attentive", 1);
--- INSERT INTO bears VALUES (4,"Sergeant Brown", 4, "F", "brown", "fearful", 1);
--- INSERT INTO bears VALUES (5,"Melissa", 5, "M", "black", "joyful", 1);
--- INSERT INTO bears VALUES (6,"Grinch", 6, "F", "white", "playful", 1);
--- INSERT INTO bears VALUES (7,"Wendy", 7, "M", "brown", "social", 1);
--- INSERT INTO bears VALUES (8,NULL, 8, "F", "black", "unknown", 1);
-
 INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
 (2,"Rowdy", 10, "M", "black", "intense", 1),
